41b65c841705f20afa911ae567239122501d4ed6
David Alger <david@classyllama.com>
2015-08-17 16:29:56 -0500

    Configuration Updates
    - Add proxy config for nginx
    - Configure httpd on correct ports
    - Add xdebug config
    - Add httpd env vars for Magento 1 & 2

--- vagrant/scripts/web.d/20-httpd.sh
+++ vagrant/scripts/web.d/20-httpd.sh
@@ -3,5 +3,6 @@ yum install -y -q httpd
 if [[ -d ./etc/httpd/conf.d/ ]]; then
     cp ./etc/httpd/conf.d/*.conf /etc/httpd/conf.d/
 fi
+perl -pi -e 's/Listen 80//' /etc/httpd/conf/httpd.conf
 
 service httpd start
--- vagrant/scripts/web.d/20-nginx.sh
+++ vagrant/scripts/web.d/20-nginx.sh
@@ -4,4 +4,4 @@ if [[ -d ./etc/httpd/conf.d/ ]]; then
     cp ./etc/nginx/conf.d/*.conf /etc/nginx/conf.d/
 fi
 
-# service nginx start
+service nginx start
