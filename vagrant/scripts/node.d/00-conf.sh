edb6295187a6e534fc1f2e7b02be5a0d814f95d0
David Alger <david@classyllama.com>
2015-08-18 10:37:58 -0500

    Fixed issue with VMWare HGFS not being available on reload
    - see mitchellh/vagrant#4362 (comment: https://github.com/mitchellh/vagrant/issues/4362#issuecomment-52589577) for details

--- vagrant/scripts/node.d/00-conf.sh
+++ vagrant/scripts/node.d/00-conf.sh
@@ -1,5 +1,12 @@
 # setup misc generic node configuration
 
+# configure VM Ware tools to automatically rebuild missing VMW kernel modules upon boot
+# see: https://github.com/mitchellh/vagrant/issues/4362#issuecomment-52589577
+#
+if [[ -f /etc/vmware-tools/locations ]]; then
+    sed -i -re 's/^answer (AUTO_KMODS_ENABLED|AUTO_KMODS_ENABLED_ANSWER) no$/answer \1 yes/' /etc/vmware-tools/locations
+fi
+
 if [[ -f ./etc/hosts ]]; then
     cp ./etc/hosts /etc/hosts
 fi
