e4e51f565462ecdb834761099668246bf22442a4
David Alger <david@classyllama.com>
2015-08-20 22:17:24 -0500

    Add bindfs support to nfs mounts

--- vagrant/lib/mount.rb
+++ vagrant/lib/mount.rb
@@ -1,16 +1,25 @@
-# Mounts a directory from the host machine on the vm at the designated path using an NFS mount
+
+# require the vagrant-bind-fs plugin or fail
+unless Vagrant.has_plugin?('vagrant-bindfs')
+  raise 'vagrant-bindfs is not installed! Please install with: vagrant plugin install vagrant-bindfs'
+end
+
+# Mounts a directory from the host machine on the vm at the designated path using an NFS mount and bind-fs
 # Params:
 # +conf+:: vagrant provisioning conf object
 # +host_path+:: +String+ full path to directory on host machine
 # +guest_path+:: +String+ mount location on virtual machine
 # +read_only+:: +Boolean+ flag to indicate whether to mount in read-only mode
-def mount_nfs (conf, id, host_path, guest_path, read_only = false)
+def mount_nfs (conf, id, host_path, guest_path, user = 'vagrant', group = 'vagrant', read_only = false)
   mount_options = 'vers=3,udp'
   if read_only == true
     mount_options = mount_options + ',ro'
   end
-  conf.vm.synced_folder host_path, guest_path, id: id, create: true, nfs_export: false, type: 'nfs',
+  unbound_path = '/var/nfs-unbound-' + id
+  conf.vm.synced_folder host_path, unbound_path, id: id, create: true, nfs_export: false, type: 'nfs',
     :nfs => { mount_options: mount_options }
+    
+  conf.bindfs.bind_folder unbound_path, guest_path, force_user: user, force_group: group, create_as_user: true
 end
 
 # Mounts directory from the host machine on the vm at the designated path using default vmfs
