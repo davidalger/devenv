3361cba944f5c02cd5dbe9b0e459c0ebc7dbef8e
David Alger <david@classyllama.com>
2015-08-20 19:06:22 -0500

    Switched NFS mounts to use the synced folder built-in

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## Host Setup
 * Add the following line to /etc/exports and restart nfsd by running `sudo nfsd restart`
 
-        "/Volumes/Server/" -alldirs -network 10.19.89.0 -mask 255.255.255.0 -mapall=0:0
+        "/Volumes/Server/" -alldirs -network 192.168.235.0 -mask 255.255.255.0 -mapall=root:wheel
 
--- vagrant/lib/mount.rb
+++ vagrant/lib/mount.rb
@@ -5,14 +5,12 @@
 # +guest_path+:: +String+ mount location on virtual machine
 # +read_only+:: +Boolean+ flag to indicate whether to mount in read-only mode
 def mount_nfs (conf, id, host_path, guest_path, read_only = false)
-  conf.vm.provision :shell, run: 'always' do |conf|
-    mount_options = 'vers=3,udp'
-    if read_only == true
-      mount_options = mount_options + ',ro'
-    end
-    conf.name = %-mount_nfs: #{id}-
-    conf.inline = %-mkdir \-p '#{guest_path}'; mount \-o '#{mount_options}' 10.19.89.1:'#{host_path}' '#{guest_path}'-
+  mount_options = 'vers=3,udp'
+  if read_only == true
+    mount_options = mount_options + ',ro'
   end
+  conf.vm.synced_folder host_path, guest_path, id: id, create: true, nfs_export: false, type: 'nfs',
+    :nfs => { mount_options: mount_options }
 end
 
 # Mounts directory from the host machine on the vm at the designated path using default vmfs
