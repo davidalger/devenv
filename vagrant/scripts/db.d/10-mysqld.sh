ba4d315d6b23296f146aaec042a540c2de7cc1a4
David Alger <david@classyllama.com>
2015-08-21 12:03:33 -0500

    Got the mysqld service initializing in a new installation

--- vagrant/scripts/db.d/10-mysqld.sh
+++ vagrant/scripts/db.d/10-mysqld.sh
@@ -5,11 +5,35 @@ if [[ -f ./etc/my.cnf ]]; then
 fi
 
 yum install -y -q mysql-server
-service mysqld start || true        # let the script proceed, even if start issues error code
 
-mysql -uroot -e "
-    GRANT ALL PRIVILEGES ON *.* TO 'root'@'10.19.89.1' WITH GRANT OPTION;
-    GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-    GRANT ALL PRIVILEGES ON *.* TO 'root'@'dev-web' WITH GRANT OPTION;
-    FLUSH PRIVILEGES;
-"
+# test for presence of ibdata1 to determine if we have a new install or not
+if [[ ! -f /var/lib/mysql/data/ibdata1 ]]; then
+    
+    # grab our mount parameters for later use and unmount the data directory
+    _mount=$(grep /var/lib/mysql/data /etc/mtab | awk '{print "mount -t "$3" -o "$4" "$1" "$2}')
+    umount /var/lib/mysql/data/
+    
+    # start servcie to initialize data directory and then stop for remount
+    service mysqld start
+    service mysqld stop
+    
+    # move aside new data directory and remount persistent data storage
+    mv /var/lib/mysql/data/ /var/lib/mysql/data.new
+    mkdir /var/lib/mysql/data
+    $_mount
+    
+    # correct ownership of new files to match mounts owner/group and move into place
+    chown -R $(stat -c '%u:%g' /var/lib/mysql/data/) /var/lib/mysql/data.new/
+    mv /var/lib/mysql/data.new/* /var/lib/mysql/data/
+    rmdir /var/lib/mysql/data.new
+    
+    # grant root mysql user privileges to connect for other vms and host machine
+    service mysqld start
+    mysql -uroot -e "
+        GRANT ALL PRIVILEGES ON *.* TO 'root'@'10.19.89.1' WITH GRANT OPTION;
+        GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+        GRANT ALL PRIVILEGES ON *.* TO 'root'@'dev-web' WITH GRANT OPTION;
+        FLUSH PRIVILEGES;
+    "
+    service mysqld stop # will be started in seperate provisioner
+fi
