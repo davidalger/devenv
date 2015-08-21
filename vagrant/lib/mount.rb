58e16a7072dd7503777c0eeb9b4e53c18203bcb8
David Alger <david@classyllama.com>
2015-08-21 13:00:19 -0500

    Setup bind of NFS mounts so absolute symlinks will correctly point at something

--- vagrant/lib/mount.rb
+++ vagrant/lib/mount.rb
@@ -5,12 +5,25 @@
 # +guest_path+:: +String+ mount location on virtual machine
 # +read_only+:: +Boolean+ flag to indicate whether to mount in read-only mode
 def mount_nfs (conf, id, host_path, guest_path, read_only = false)
+  
+  # compute final list of nfs mount options
   mount_options = 'vers=3,udp'
   if read_only == true
     mount_options = mount_options + ',ro'
   end
+  
+  # configure the nfs mount using the built-in
   conf.vm.synced_folder host_path, guest_path, id: id, create: true, nfs_export: false, type: 'nfs',
     :nfs => { mount_options: mount_options }
+  
+  # bind host path on guest so absolute symlinks on host will correctly point to a valid location
+  conf.vm.provision :shell, run: 'always' do |conf|
+    conf.name = 'binding ' + guest_path
+    conf.inline = %-
+      mkdir \-p #{host_path}
+      sudo mount \-o bind #{guest_path} #{host_path}
+    -
+  end
 end
 
 # Mounts directory from the host machine on the vm at the designated path using default vmfs
@@ -20,6 +33,6 @@ end
 # +guest_path+:: +String+ mount location on virtual machine
 # +mount_options+:: +Array+ additional mount options
 def mount_vmfs (conf, id, host_path, guest_path, mount_options: [])
-  FileUtils.mkdir_p host_path
+  FileUtils.mkdir_p host_path   # ensure path on host is present
   conf.vm.synced_folder host_path, guest_path, id: id, mount_options: mount_options
 end
