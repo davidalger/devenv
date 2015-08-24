bc2d94c58ffdd32191cf07b1d82842da8223d9f6
David Alger <david@classyllama.com>
2015-08-24 12:57:44 -0500

    Move aside the noindex.html file so lack of configured error pages don't result in this being served up

--- vagrant/scripts/web.d/20-httpd.sh
+++ vagrant/scripts/web.d/20-httpd.sh
@@ -6,3 +6,7 @@ if [[ -d ./etc/httpd/conf.d/ ]]; then
     cp ./etc/httpd/conf.d/*.conf /etc/httpd/conf.d/
 fi
 perl -pi -e 's/Listen 80//' /etc/httpd/conf/httpd.conf
+
+if [[ -f "/var/www/error/noindex.html" ]]; then
+    mv /var/www/error/noindex.html /var/www/error/noindex.html.disabled
+fi
