295494cb03a2dc774dc1e25e3b22e103d7996f6a
David Alger <david@classyllama.com>
2015-08-25 18:04:00 -0500

    Add restart of nfslock service on db node boot sequence
    The restart of the nfslock service enables mysqld to start properly after a halt/destroy/boot sequence. Without restarting the nfslock service, the InnoDB engine will fill the log with "Unable to lock ./ibdata1, error: 37" messages and ultimately fail to start until nfslock is restarted.

--- vagrant/vagrant.rb
+++ vagrant/vagrant.rb
@@ -56,6 +56,7 @@ Vagrant.configure(2) do |conf|
     mount_nfs(node, '-mysql-data', BASE_DIR + '/mysql/data', '/var/lib/mysql/data')
     
     bootstrap_sh(node, ['node', 'db'])
+    service(node, 'nfslock', 'restart')
     service(node, 'mysqld', 'start')
   end
   
