a0955cea26092832e0dd3780ecbd17894ae0da98
David Alger <david@classyllama.com>
2015-08-28 11:41:36 -0500

    Resolve #18: Fixes issues with m2.dev provisioning both failing and taking an exceedingly long time to run due to timeouts

--- vagrant/scripts/sites.d/10-m2.dev.sh
+++ vagrant/scripts/sites.d/10-m2.dev.sh
@@ -2,13 +2,19 @@
 set -e
 wd=$(pwd)
 
+# use a bare clone to keep up-to-date local mirror of master
 if [[ ! -d "$CACHE_DIR/m2.repo" ]]; then
-    git clone --mirror -q "https://github.com/magento/magento2.git" "$CACHE_DIR/m2.repo"
+    echo "Cloning remote repository to local mirror. This could take a while..."
+    git clone --bare -q "https://github.com/magento/magento2.git" "$CACHE_DIR/m2.repo"
+    cd "$CACHE_DIR/m2.repo"
+    git remote add origin "https://github.com/magento/magento2.git"
+    git fetch -q
 else
     cd "$CACHE_DIR/m2.repo"
     git fetch -q || true
 fi
 
+# install or update codebase from local mirror
 if [[ ! -d "$SITES_DIR/m2.dev" ]]; then
     echo "Setting up site from scratch. This could take a while..."
     
@@ -17,8 +23,23 @@ if [[ ! -d "$SITES_DIR/m2.dev" ]]; then
 
     cd "$SITES_DIR/m2.dev"
     composer install -q --prefer-dist
-    mysql -e 'create database m2_dev'
+    mkdir -p /server/_var/m2.dev
+    ln -s "/server/_var/m2.dev/{session,log,generation,composer_home,view_preprocessed}" "var/"
 
+    >&2 echo "Note: please add a record to your /etc/hosts file for m2.dev and re-run the vhost generator"
+else
+    cd "$SITES_DIR/m2.dev"
+    git pull -q
+    rm -rf "var/{session,log,generation,composer_home,view_preprocessed}/*"
+    composer install -q --prefer-dist
+fi
+
+# either install or upgrade database
+code=
+mysql -e 'use m2_dev' || code="$?"
+if [[ $code ]]; then
+    mysql -e 'create database m2_dev'
+    
     bin/magento setup:install -q \
         --admin-user=admin \
         --admin-firstname=Admin \
@@ -28,14 +49,7 @@ if [[ ! -d "$SITES_DIR/m2.dev" ]]; then
         --db-host=dev-db \
         --db-user=root \
         --db-name="m2_dev"
-    
-    >&2 echo "Note: please add a record to your /etc/hosts file for m2.dev and re-run the vhost generator"
 else
-    cd "$SITES_DIR/m2.dev"
-    git pull -q
-
-    cd "$SITES_DIR/m2.dev"
-    composer install -q
     bin/magento setup:upgrade -q
 fi
 
