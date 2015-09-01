cc3d38c198ab1813efa8f9e6be5a3d5b2e47c8db
David Alger <david@classyllama.com>
2015-09-01 16:49:15 -0500

    Reorganized everything into one class per package/manager

--- bin/glowbot.py
+++ bin/glowbot.py
@@ -45,25 +45,8 @@ def main():
         tmp = tf.mkdtemp('-' + sc_name)
     print 'tmp: ' + tmp + '\n'
 
-    # Install Command Line Tools if missing
-    sh.ohai('Verifying presence of Command Line Tools')
-    if sh.call_out(['/bin/sh', '-c', 'xcode-select -p 2> /dev/null'])['result'] is not 0:
-        sh.ohay('Tools not found, hunting packages')
-
-        # sets internal flag which causes software update to look for CLI tool packages
-        sh.call('touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress')
-
-        # retrieve available CLI tool packages to install
-        ver = sh.call_out(['/bin/sh', '-c', "softwareupdate -l | grep '* Command Line'"])['output'][0]
-        ver = ver.split('\n')[0].strip(' *')  # grab first item (in case of beta OS)
-        sh.ohay('Found "%s"' % ver)
-
-        # install the package
-        sh.ohay('Installing via Software Update')
-        sh.call(['softwareupdate', '-i', ver, '-v'])
-
     # initiate package installation
-    PackageInstaller().run()
+    Manifest().process()
 
 
 class Shell:
@@ -104,127 +87,106 @@ class Shell:
         print '\x1B[4;31mError\x1B[0;31m:\x1B[0;39m %s\x1B[0;0m' % msg
 
 
-class Package:
-    name = str
-    type = str
 
-    def __init__(self, name, type=None):
-        self.name = name
-        self.type = type
+class CommandLineTools:
 
-    def get_desc(self):
-        if self.type is not None:
-            return self.type + ':' + self.name
-        return self.name
+    shell = Shell()
 
-
-class PackageInstaller:
     def __init__(self):
         pass
 
+    def describe(self):
+        return 'command line tools'
+
+    def check(self):
+        if self.shell.call_out(['/bin/sh', '-c', 'xcode-select -p 2> /dev/null'])['result'] is not 0:
+            return False
+        return True
+    
+    def install(self):
+        # sets internal flag which causes software update to look for CLI tool packages
+        self.shell.call('touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress')
+
+        # retrieve available CLI tool packages to install
+        ver = self.shell.call_out(['/bin/sh', '-c', "softwareupdate -l | grep '* Command Line'"])['output'][0]
+        ver = ver.split('\n')[0].strip(' *')  # grab first item (in case of beta OS)
+        self.shell.ohay('Found "%s"' % ver)
+
+        # install the package
+        self.shell.ohay('Installing via Software Update')
+        self.shell.call(['softwareupdate', '-i', ver, '-v'])
+        
+
+class Brew:
+
     shell = Shell()
-    packages = [
-        Package('brew'),
-        Package('ack', 'brew'),
-        Package('composer', 'brew'),
-        Package('figlet', 'brew'),
-        Package('gettext', 'brew'),
-        Package('git', 'brew'),
-        Package('git-flow', 'brew'),
-        Package('hub', 'brew'),
-        Package('iperf', 'brew'),
-        Package('jmeter', 'brew'),
-        Package('md5sha1sum', 'brew'),
-        Package('neon', 'brew'),
-        Package('pcre', 'brew'),
-        Package('readline', 'brew'),
-        Package('redis', 'brew'),
-        Package('rename', 'brew'),
-        Package('serf', 'brew'),
-        Package('siege', 'brew'),
-        Package('sloccount', 'brew'),
-        Package('sqlite', 'brew'),
-        Package('squid', 'brew'),
-        Package('subversion', 'brew'),
-        Package('tree', 'brew'),
-        Package('varnish', 'brew'),
-        Package('wakeonlan', 'brew'),
-        Package('watch', 'brew'),
-        Package('wget', 'brew'),
-        Package('zlib', 'brew'),
-        Package('textmate'),
-        Package('dropbox'),
-        Package('server'),
-        Package('sites'),
-    ]
+    packages = []
+    
+    def __init__(self, packages):
+        self.packages = packages
 
-    def run(self):
+    def describe(self):
+        return 'brew'
+        
+    def check(self):
+        if os.path.exists('/usr/local/bin/brew') is False:
+            self.shell.ohay('brew not installed')
+            return False
+    
         for pkg in self.packages:
-            print
-            self.shell.ohai('Checking for %s package...' % pkg.get_desc())
-            if self.installed(pkg) == False:
-                self.shell.ohay('Installing package %s...' % pkg.get_desc())
-                result = self.pkg_get_method('ins', pkg)(pkg)
-                if result == False:
-                    self.shell.error('Installation of package %s failed.' % pkg.get_desc())
-                    exit(1)
+            if self.check_keg(pkg) is False:
+                self.shell.ohay('missing %s' % pkg)
+                return False
+        return True
+        
+    def install(self):
+        if os.path.exists('/usr/local/bin/brew') is False:
+            self.shell.call([
+                '/bin/sh', '-c',
+                'curl -fsSkL https://raw.githubusercontent.com/Homebrew/install/master/install | ruby'
+            ])
+            self.shell.ohai('Running brew doctor')
+
+            output = self.shell.call_out('brew doctor')['output']
+            if output[0] != 'Your system is ready to brew.\n':
+                for line in output:
+                    if line is not None:
+                        print line
+                self.shell.ohai('Please resolve the issues brew doctor reported and try again.')
+                exit(1)
             else:
-                # TODO true reply from .installed sometimes means "Error: No available formula for ..."
-                self.shell.ohay('Package %s is installed.' % pkg.get_desc())
+                print 'Your system is ready to brew.'
+        
+        for pkg in self.packages:
+            if self.check_keg(pkg) is False:
+                self.shell.ohay('tapping %s' % pkg)
+                self.shell.call('/usr/local/bin/brew install %s' % pkg)
 
-    def installed(self, pkg):
-        return self.pkg_get_method('check', pkg)(pkg)
+    def manager(self):
+        return True
+        
+    def check_keg(self, name):
+        if self.shell.call(['/bin/sh', '-c', 'brew list %s > /dev/null 2>&1' % name])['result'] is 1:
+            return False
+        return True
 
-    def pkg_get_method(self, op_type, pkg):
-        if pkg.type is not None:
-            op_type = '%s_%s' % (pkg.type, op_type)
-        return getattr(self, 'pkg_%s_%s' % (op_type, pkg.name), getattr(self, 'pkg_%s' % op_type))
+class Textmate:
 
-    def pkg_ins(self, pkg):
-        self.shell.error('Missing pkg_ins method for %s package.' % pkg.get_desc())
-        exit(1)
+    shell = Shell()
 
-    def pkg_check(self, pkg):
-        self.shell.error('Missing pkg_check method for %s package.' % pkg.get_desc())
-        exit(1)
+    def __init__(self):
+        pass
 
-    def pkg_ins_brew(self, pkg):
-        self.shell.call([
-            '/bin/sh', '-c',
-            'curl -fsSkL https://raw.githubusercontent.com/Homebrew/install/master/install | ruby'
-        ])
-        self.shell.ohai('Running brew doctor')
+    def describe(self):
+        return 'textmate'
 
-        output = self.shell.call_out('brew doctor')['output']
-        if output[0] != 'Your system is ready to brew.\n':
-            for line in output:
-                if line is not None:
-                    print line
-            self.shell.ohai('Please resolve the issues brew doctor reported and try again.')
-            exit(1)
-        else:
-            print 'Your system is ready to brew.'
-
-    def pkg_check_brew(self, pkg):
-        if os.path.exists('/usr/local/bin/brew'):
+    def check(self):
+        if os.path.exists('/Applications/TextMate.app'):
             return True
         return False
 
-    def pkg_brew_ins(self, pkg):
-        self.shell.call('/usr/local/bin/brew install %s' % pkg.name)
-
-    def pkg_brew_check(self, pkg):
-        output = self.shell.call_out('/usr/local/bin/brew info %s' % pkg.name)['output'][0].split('\n')
-        for line in output:
-            if line == 'Not installed':
-                return False
-            elif line.find('Error:') != -1:
-                self.shell.ohai(line)
-                exit(1)
-        return True
-
-    def pkg_ins_textmate(self, pkg):
-        print 'Finding the latest version'
+    def install(self):
+        print 'Finding latest version'
         release = 'https://api.textmate.org/downloads/release'
 
         print 'Downloading ' + release
@@ -240,12 +202,22 @@ class PackageInstaller:
         self.shell.call('/usr/bin/defaults write com.macromates.TextMate mateInstallPath /usr/local/bin/mate')
         self.shell.call('/usr/bin/defaults write com.macromates.TextMate mateInstallVersion ' + mate_ver)
 
-    def pkg_check_textmate(self, pkg):
-        if os.path.exists('/Applications/TextMate.app'):
+class Dropbox:
+
+    shell = Shell()
+
+    def __init__(self):
+        pass
+
+    def describe(self):
+        return 'dropbox'
+
+    def check(self):
+        if os.path.exists('/Applications/DropBox.app'):
             return True
         return False
 
-    def pkg_ins_dropbox(self, pkg):
+    def install(self):
         print 'Downloading Dropbox for Mac'
         tmpFile = self.shell.curl_download('https://www.dropbox.com/download?plat=mac', 'dropbox.dmg')
 
@@ -265,12 +237,36 @@ class PackageInstaller:
 
         print 'Installation complete!'
 
-    def pkg_check_dropbox(self, pkg):
-        if os.path.exists('/Applications/DropBox.app'):
-            return True
-        return False
+class Shortcuts:
 
-    def pkg_ins_sites(self, pkg):
+    shell = Shell()
+
+    def __init__(self):
+        pass
+
+    def describe(self):
+        return 'shortcuts'
+
+    def check(self):
+        if os.path.exists('/server') == False:
+            return False
+
+        if os.path.exists('/sites') == False:
+            return False
+
+        return True
+
+    def install(self):
+        print 'Softlinking /server to /Volumes/Server'
+        if os.path.exists('/Volumes/Server') == False:
+            self.shell.warn('Can not find /Volumes/Server')
+            return False
+
+        result = self.shell.call('sudo /bin/ln -s /Volumes/Server /server')['result']
+        if result != 0:
+            self.shell.warn('Installation of %s failed with error code: %d' % (pkg.describe(), result))
+            return False
+
         print 'Softlinking /sites to /server/sites'
         if os.path.exists('/Volumes/Server/sites') == False:
             if os.path.exists('/Volumes/Server'):
@@ -281,29 +277,64 @@ class PackageInstaller:
 
         result = self.shell.call('sudo /bin/ln -s /Volumes/Server/sites /sites')['result']
         if result != 0:
-            self.shell.warn('Installation of %s failed with error code: %d' % (pkg.get_desc(), result))
+            self.shell.warn('Installation of %s failed with error code: %d' % (pkg.describe(), result))
             return False
 
-    def pkg_check_sites(self, pkg):
-        if os.path.exists('/sites'):
-            return True
-        return False
+class Manifest:
+    def __init__(self):
+        pass
 
-    def pkg_ins_server(self, pkg):
-        print 'Softlinking /server to /Volumes/Server'
-        if os.path.exists('/Volumes/Server') == False:
-            self.shell.warn('Can not find /Volumes/Server')
-            return False
+    shell = Shell()
+    packages = [
+        CommandLineTools(),
+        Brew([
+            'ack',
+            'composer',
+            'figlet',
+            'gettext',
+            'git',
+            'git-flow',
+            'hub',
+            'iperf',
+            'jmeter',
+            'md5sha1sum',
+            'mysql',
+            'neon',
+            'pcre',
+            'pv',
+            'readline',
+            'redis',
+            'rename',
+            'serf',
+            'siege',
+            'sloccount',
+            'sqlite',
+            'squid',
+            'subversion',
+            'tree',
+            'varnish',
+            'wakeonlan',
+            'watch',
+            'wget',
+            'zlib'
+        ]),
+        Textmate(),
+        Dropbox(),
+        Shortcuts(),
+    ]
 
-        result = self.shell.call('sudo /bin/ln -s /Volumes/Server /server')['result']
-        if result != 0:
-            self.shell.warn('Installation of %s failed with error code: %d' % (pkg.get_desc(), result))
-            return False
-
-    def pkg_check_server(self, pkg):
-        if os.path.exists('/server'):
-            return True
-        return False
+    def process(self):
+        for pkg in self.packages:
+            print
+            self.shell.ohai('Checking %s package' % pkg.describe())
+            if pkg.check() == False:
+                self.shell.ohay('Installing %s package' % pkg.describe())
+                result = pkg.install()
+                if result == False:
+                    self.shell.error('Installation of package %s failed' % pkg.describe())
+                    exit(1)
+            else:
+                self.shell.ohay('Package %s is installed.' % pkg.describe())
 
 if __name__ == '__main__':
     main()
