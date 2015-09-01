davidalger/devenv

commit cfe3a17b96d9cfb73d94edabf6121824c328cd96

Fixed bug breaking vagrant runs if run from within /server/vagrant

Vagrantfile
@@ -2,7 +2,7 @@
 # vi: set ft=ruby :
 
 if File.symlink?(__FILE__)
-  VAGRANT_FILE = File.readlink(__FILE__)
+  VAGRANT_FILE = File.absolute_path(File.readlink(__FILE__))
 else
   VAGRANT_FILE = __FILE__
 end

vagrant/Vagrantfile
@@ -1,1 +1,1 @@
-/server/Vagrantfile
+../Vagrantfile

commit 18c146eab4ba0bdec033cbf5d9827281b1b07a74

Fixed regression caused by relative links introduced in cfe3a17

Vagrantfile
@@ -2,9 +2,13 @@
 # vi: set ft=ruby :
 
 if File.symlink?(__FILE__)
-  VAGRANT_FILE = File.absolute_path(File.readlink(__FILE__))
+  vagrant_file = File.absolute_path(File.readlink(__FILE__))
+  if File.symlink?(File.dirname(vagrant_file))
+    vagrant_file = File.readlink(File.dirname(vagrant_file)) + '/Vagrantfile'
+  end
 else
-  VAGRANT_FILE = __FILE__
+  vagrant_file = __FILE__
 end
+VAGRANT_FILE = vagrant_file
 
 require_relative 'vagrant/vagrant'

vagrant/Vagrantfile
@@ -1,1 +1,1 @@
-../Vagrantfile
+/server/Vagrantfile