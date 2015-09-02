0259b34e067f054cbeba0a764fb8d9a2d2cd7338
David Alger <david@classyllama.com>
2015-09-02 11:09:58 -0500

    Add general bash profile stuff to profile.d scripts

---
+++ vagrant/etc/profile.d/alias.sh
@@ -0,0 +1,56 @@
+
+########################################
+# host specific aliases
+
+if [ -f "/etc/.vagranthost" ]; then
+    
+    # general shortcuts
+    alias vcd='vagrant ssh -c "[ -d $(pwd) ] && cd $(pwd); bash"'
+    alias st="open -a sourcetree"
+    if [ -x /System/Library/CoreServices/Applications/Network\ Utility.app/Contents/Resources/stroke ]; then
+        alias stroke="/System/Library/CoreServices/Applications/Network\ Utility.app/Contents/Resources/stroke"
+    fi
+    if [ -x "$(which dtrace)" ]; then
+        alias iotrace="sudo dtrace -n 'syscall::open*:entry { printf(\"%s %s\",execname,copyinstr(arg0)); }'"
+    fi
+
+fi
+
+########################################
+# vm specific aliases
+# if [ ! -f "/etc/.vagranthost" ]; then
+    # vm specific aliases should be added here
+# fi
+
+########################################
+# generic aliases
+
+# generic shortcuts
+alias php-debug="php -d xdebug.remote_autostart=on"
+alias trail="tail -f"
+
+# file manipulation
+alias crlf-to-lf="find ./ -type f | grep -vE '.svn/|.git/' | xargs -n1 perl -p -i -e 's/\r\n/\n/g'"
+alias rsyncf="rsync --exclude-from ~/.rsync_excludes"
+alias udiff="diff -urB \$1 \$2 | grep -v 'Only in' | grep -v 'diff ' | sed -E 's#(---|\+\+\+) /.*/htdocs/(.*)#\1 \2#g'"
+alias mdiff="diff -Bbwr -I ' \*.*'"
+
+# magento shortcuts
+alias mreports="grep -rE ^a: var/report/ | cut -d '#' -f 1 | cut -d ';' -f 2 | sort | uniq -c"
+alias mexceptions='ack "^Exception" "$1" | sort | uniq -c | sort -nr | vi -c "set nowrap" -'
+# alias flush-cache-storage="php -r \"require_once 'app/Mage.php'; umask(0); Mage::app()->getCacheInstance()->flush();\""
+# alias flush-cache="php -r \"require_once 'app/Mage.php'; umask(0); Mage::app()->cleanCache();\""
+# alias cron-reset="find var/cache/ -type f -name '*CRON*' -print0 | xargs -0 rm"
+
+# setup git aliases if they do not exist
+if [ "$(git config --global --get alias.permission-reset)" = "" ]; then
+    git config --global --add alias.permission-reset \
+        '!git diff -p -R | grep -E "^(diff|(old|new) mode)" | git apply'
+fi
+
+# TODO convert these to git aliases
+alias git-prune-remote-branches='git branch -r --merged | grep -v develop | grep -v master | grep origin | grep -v "$(git branch | grep \* | cut -d " " -f2)" | grep -v ">" | xargs -L1 | cut -d "/" -f2-5 | xargs git push origin --delete'
+alias git-prune-local-branches='git branch --merged | grep -v develop | grep -v master | grep -v "$(git branch | grep \* | cut -d " " -f2)" | grep -v ">" | xargs -L1 | xargs -n1 git branch -d'
+
+# timestamp for use in file names
+alias ts="date +%F_%H-%M-%S"
--- vagrant/etc/profile.d/env.sh
+++ vagrant/etc/profile.d/env.sh
@@ -1,6 +1,30 @@
 # set environemnt variables
 
+# set magento developer mode env vars
 export MAGE_IS_DEVELOPER_MODE=1
 export MAGE_MODE=developer
+
+# set central composer home
 export COMPOSER_HOME=/server/.cache/composer
-export PATH="/usr/local/bin:$PATH"
+
+# configure PATH to use local and user scripts
+export PATH=~/bin:/usr/local/bin:$PATH:/usr/local/sbin
+
+# use textmate as editor if mate cli tool is present
+[ -x /usr/local/bin/mate ] && export EDITOR="/usr/local/bin/mate -w"
+
+# textmate support path if present
+if [ -d "/Applications/TextMate.app/Contents/SharedSupport/Support" ]; then
+    export TM_SUPPORT_PATH='/Applications/TextMate.app/Contents/SharedSupport/Support'
+fi
+
+# brew completion if present
+if [ -x "$(which brew)" ] && [ -f $(brew --prefix)/etc/bash_completion ]; then
+  . $(brew --prefix)/etc/bash_completion
+fi
+
+# git completion
+[ -f ~/.git-completion.bash ] && . ~/.git-completion.bash
+
+# setup rvm if present
+[ -s "$HOME/.rvm/scripts/rvm" ] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
