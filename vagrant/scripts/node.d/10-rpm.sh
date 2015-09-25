4ea21bd471df0b741568a235b260cef3cc67ed5a
David Alger <david@classyllama.com>
2015-09-25 12:31:14 -0500

    Update version of Nginx for HTTP 1.1 transfer encoding support in proxy_pass (resolves #37)

--- vagrant/scripts/node.d/10-rpm.sh
+++ vagrant/scripts/node.d/10-rpm.sh
@@ -3,23 +3,22 @@ set -e
 
 source ./scripts/lib/rpm.sh
 
-echo "Setting up yum cache"
 if [[ -f ./etc/yum.conf ]]; then
     cp ./etc/yum.conf /etc/yum.conf
 fi
 
-echo "Importing RPM GPG Keys"
 rpm --import ./etc/keys/RPM-GPG-KEY-CentOS-6.txt
 rpm --import ./etc/keys/RPM-GPG-KEY-EPEL-6.txt
 rpm --import ./etc/keys/RPM-GPG-KEY-MySql.txt
 rpm --import ./etc/keys/RPM-GPG-KEY-remi.txt
+rpm --import ./etc/keys/RPM-GPG-KEY-nginx.txt
 
-echo "Installing EPEL repository"
 yum install -y epel-release
 
-echo "Installing Remi's RPM repository"
 install_rpm http://rpms.famillecollet.com/enterprise/remi-release-6.rpm \
     /var/cache/yum/rpms/remi-release-6.rpm
 
-echo "Updating installed software"
+install_rpm http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm \
+    /var/cache/yum/rpms/nginx-release-6.rpm
+
 yum update -y -q
