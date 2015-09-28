e9f53278531da907835d5df5c6e2e46a3bed9549
David Alger <david@classyllama.com>
2015-09-28 17:52:29 -0500

    Add sample data support to m2site.sh script

--- vagrant/bin/m2site.sh
+++ vagrant/bin/m2site.sh
@@ -1,9 +1,50 @@
-# install a sandbox m2.dev site
+# install a magento 2 site
 set -e
-wd=$(pwd)
+wd="$(pwd)"
 
 SHARED_DIR=/server/.shared
 SITES_DIR=/server/sites
+
+HOSTNAME=m2.dev
+SAMPLEDATA=
+
+## argument parsing
+
+for arg in "$@"; do
+    case $arg in
+        --hostname=*)
+            HOSTNAME="${arg#*=}"
+            if [[ ! "$HOSTNAME" =~ ^[a-z0-9]+\.[a-z]{2,5}$ ]]; then
+                >&2 echo "Error: Invalid value given --hostname=$HOSTNAME"
+                exit -1
+            fi
+            ;;
+        -d|--sampledata)
+            SAMPLEDATA=1
+            ;;
+        --help)
+            echo "Usage: $(basename $0) [-d] [--sampledata] [--hostname=example.dev]"
+            echo ""
+            echo "  -d : --sampledata             triggers installation of sample data"
+            echo "       --hostname=<hostname>    domain of the site (defaults to m2.dev)"
+            echo ""
+            exit -1
+            ;;
+        *)
+            >&2 echo "Error: Unrecognized argument $arg"
+            ;;
+    esac
+done
+
+DB_NAME="$(printf "$HOSTNAME" | tr . _)"
+
+# sampledata flag to prevent re-running the sampledata:install routine
+SAMPLEDATA_INSTALLED=$SITES_DIR/$HOSTNAME/var/.sampledata
+if [[ $SAMPLEDATA && -f $SAMPLEDATA_INSTALLED ]]; then
+    SAMPLEDATA=
+fi
+
+## verify pre-requisites
 
 if [[ -f /etc/.vagranthost ]]; then
     >&2 echo "Error: This script should be run from within the vagrant machine. Please vagrant ssh, then retry"
@@ -12,49 +53,98 @@ fi
 
 php_version=$(php -r 'echo phpversion();' | cut -d . -f2)
 if [[ $php_version < 5 ]]; then
-    echo "Error: Magento 2 requires PHP 5.5 or newer"
+    >&2 echo "Error: Magento 2 requires PHP 5.5 or newer"
     exit
 fi
 
 # use a bare clone to keep up-to-date local mirror of master
-if [[ ! -d "$SHARED_DIR/m2.repo" ]]; then
-    echo "Cloning remote repository to local mirror. This could take a while..."
-    git clone --bare -q "https://github.com/magento/magento2.git" "$SHARED_DIR/m2.repo"
-    cd "$SHARED_DIR/m2.repo"
-    git remote add origin "https://github.com/magento/magento2.git"
-    git fetch -q
-else
-    echo "Updating local magento2 repository mirror"
-    cd "$SHARED_DIR/m2.repo"
-    git fetch -q || true
-fi
+function mirror_repo {
+    wd="$(pwd)"
+    
+    repo_url="$1"
+    mirror_path="$2"
+    
+    if [[ ! -d "$mirror_path" ]]; then
+        echo "==> Mirroring $repo_url -> $mirror_path"
+        git clone --bare -q "$repo_url" "$mirror_path"
+        cd "$mirror_path"
+        git remote add origin "$repo_url"
+        git fetch -q
+    else
+        echo "==> Updating mirror $mirror_path"
+        cd "$mirror_path"
+        git fetch -q || true
+    fi
+    
+    cd "$wd"
+}
 
 # install or update codebase from local mirror
-if [[ ! -d "$SITES_DIR/m2.dev" ]]; then
-    echo "Setting up site from scratch. This could take a while..."
+function clone_or_update {
+    wd="$(pwd)"
+    
+    repo_url="$1"
+    dest_path="$2"
+    
+    if [[ ! -d "$dest_path" ]]; then
+        echo "==> Cloning $repo_url -> $dest_path"
 
-    mkdir -p "$SITES_DIR/m2.dev"
-    git clone -q "$SHARED_DIR/m2.repo" "$SITES_DIR/m2.dev"
+        mkdir -p "$dest_path"
+        git clone -q "$repo_url" "$dest_path"
 
-    cd "$SITES_DIR/m2.dev"
-else
-    echo "Updating site from mirror"
-    cd "$SITES_DIR/m2.dev"
-    git pull -q
-fi
+        cd "$dest_path"
+    else
+        echo "Updating $dest_path from mirror"
+        cd "$dest_path"
+        git pull -q
+    fi
+    
+    cd "$wd"
+}
+
+# runs the install routine for sample data if enabled
+function install_sample_data {
+    tools_dir=$SITES_DIR/$HOSTNAME/var/tmp/m2-data/dev/tools
+    
+    # grab sample data assets
+    mirror_repo https://github.com/magento/magento2-sample-data.git $SHARED_DIR/m2-data.repo
+    clone_or_update $SHARED_DIR/m2-data.repo $SITES_DIR/$HOSTNAME/var/tmp/m2-data
+    php -f $tools_dir/build-sample-data.php -- --ce-source=$SITES_DIR/$HOSTNAME
+    
+    if [[ ! -L $SITES_DIR/$HOSTNAME/pub/pub/media/styles.css ]]; then
+        echo "==> Fixing sample data stylesheet location bug... (see issue #2)"
+        mkdir -p $SITES_DIR/$HOSTNAME/pub/pub/media
+        ln -s ../../media/styles.css $SITES_DIR/$HOSTNAME/pub/pub/media/styles.css
+    fi
+
+    echo "==> Installing sample data"
+    bin/magento setup:upgrade -q
+    bin/magento sampledata:install admin
+    touch $SAMPLEDATA_INSTALLED
+    php -f $tools_dir/build-sample-data.php -- --ce-source=$SITES_DIR/$HOSTNAME --command unlink
+}
+
+## begin execution
+
+# grab magento 2 codebase
+mirror_repo https://github.com/magento/magento2.git $SHARED_DIR/m2.repo
+clone_or_update $SHARED_DIR/m2.repo $SITES_DIR/$HOSTNAME
 
 # install all dependencies in prep for setup / upgrade
-echo "Installing composer dependencies"
+echo "==> Installing composer dependencies"
+cd $SITES_DIR/$HOSTNAME
 composer install -q --no-interaction --prefer-dist
 
 # either install or upgrade database
 code=
-mysql -e 'use m2_dev' 2> /dev/null || code="$?"
+mysql -e "use $DB_NAME" 2> /dev/null || code="$?"
 if [[ $code ]]; then
-    echo "Initializing database via setup:install"
-    mysql -e 'create database m2_dev'
+    echo "==> Creating $DB_NAME database"
+    mysql -e "create database $DB_NAME"
     
+    echo "==> Running bin/magento setup:install"
     bin/magento setup:install -q \
+        --base-url=http://$HOSTNAME \
         --backend-frontname=backend \
         --admin-user=admin \
         --admin-firstname=Admin \
@@ -63,17 +153,28 @@ if [[ $code ]]; then
         --admin-password=A123456 \
         --db-host=dev-db \
         --db-user=root \
-        --db-name="m2_dev"
+        --db-name=$DB_NAME
+    
+    if [[ $SAMPLEDATA ]]; then
+        install_sample_data
+    fi
 else
-    echo "Running setup:upgrade"
+    echo "==> Database $DB_NAME already exists"
+    echo "==> Running bin/magento setup:upgrade"
     bin/magento setup:upgrade -q
+    
+    if [[ $SAMPLEDATA ]]; then
+        install_sample_data
+    fi
 fi
 
-echo "Flushing file caches and redis service"
+echo "==> Removing var directories"
 rm -rf var/{cache,page_cache,session,log,generation,composer_home,view_preprocessed}/*
+
+echo "==> Flushing redis service"
 redis-cli flushall > /dev/null
 
-echo "Running virtual host configuration"
+echo "==> Updating virtual hosts"
 /server/vagrant/bin/vhosts.sh > /dev/null
 
 cd "$wd"
