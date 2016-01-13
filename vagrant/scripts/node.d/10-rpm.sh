532764c48c6af9e18960836f06b2049d3f28ee78
David Alger <david@classyllama.com>
2016-01-13 11:46:23 -0600

    Updated method of rpm installation to improve offline vm provisioning and allow rpm definitions to update

--- vagrant/scripts/node.d/10-rpm.sh
+++ vagrant/scripts/node.d/10-rpm.sh
@@ -12,8 +12,7 @@
 # configure rpms we need for installing current package versions
 
 set -e
-
-source ./scripts/lib/rpm.sh
+wd="$(pwd)"
 
 if [[ -f ./etc/yum.conf ]]; then
     cp ./etc/yum.conf /etc/yum.conf
@@ -26,16 +25,28 @@ rpm --import ./etc/keys/RPM-GPG-KEY-remi.txt
 rpm --import ./etc/keys/RPM-GPG-KEY-nginx.txt
 rpm --import ./etc/keys/RPM-GPG-KEY-Varnish.txt
 
-yum install -y epel-release
 yum install -y wget
 
-install_rpm http://rpms.famillecollet.com/enterprise/remi-release-6.rpm \
-    /var/cache/yum/rpms/remi-release-6.rpm
+if [[ ! -d /var/cache/yum/rpms ]]; then
+    mkdir -p /var/cache/yum/rpms
+fi
+cd /var/cache/yum/rpms
 
-install_rpm http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm \
-    /var/cache/yum/rpms/nginx-release-6.rpm
+# redirect stderr -> stdin so info is logged
+# ignore error codes for offline cache (where file does not exist the following commands should fail the script instead)
+wget --timestamp http://rpms.famillecollet.com/enterprise/remi-release-6.rpm 2>&1 || true
+wget --timestamp http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm 2>&1 || true
+wget --timestamp https://repo.varnish-cache.org/redhat/varnish-4.1.el6.rpm 2>&1 || true
 
-install_rpm https://repo.varnish-cache.org/redhat/varnish-4.1.el6.rpm \
-    /var/cache/yum/rpms/varnish-4.1el6.rpm
+rpm --checksig remi-release-6.rpm
+rpm --checksig nginx-release-centos-6-0.el6.ngx.noarch.rpm
+rpm --checksig varnish-4.1.el6.rpm
+
+yum install -y epel-release
+yum install -y remi-release-6.rpm
+yum install -y nginx-release-centos-6-0.el6.ngx.noarch.rpm
+yum install -y varnish-4.1.el6.rpm
 
 yum update -y -q
+
+cd "$wd"
