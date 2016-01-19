f08f7c5385d88dd2be85e06021261fb9944daaea
David Alger <david@classyllama.com>
2016-01-19 16:53:20 -0600

    Fixed bug breaking use of / in m2setup.sh --urlpath value

--- vagrant/bin/m2setup.sh
+++ vagrant/bin/m2setup.sh
@@ -74,7 +74,7 @@ for arg in "$@"; do
             ;;
         --urlpath=*)
             URLPATH="${arg#*=}"
-            if [[ ! "$URLPATH" =~ ^[a-z0-9][a-z0-9/]?[a-z0-9]+$ ]]; then
+            if [[ ! "$URLPATH" =~ ^[a-z0-9][a-z0-9/]*[a-z0-9]+$ ]]; then
                 >&2 echo "Error: Invalid value given --urlpath=$URLPATH"
                 exit -1
             fi
