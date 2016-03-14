0e8fe32fb96cbb34594de93dd3a2073906104fe3
David Alger <david@classyllama.com>
2016-03-14 14:07:45 -0500

    Refactored sold provisioner (ha! )

--- vagrant/scripts/solr.sh
+++ vagrant/scripts/solr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ##
- # Copyright © 2015 by David Alger. All rights reserved
+ # Copyright © 2016 by David Alger. All rights reserved
  # 
  # Licensed under the Open Software License 3.0 (OSL-3.0)
  # See included LICENSE file for full text of OSL-3.0
@@ -10,8 +10,5 @@
 
 set -e
 
-########################################
-# install and configure java
-
-########################################
-# install and configure solr service
+# TODO: install and configure java
+# TODO: install and configure solr service
